9d7f21b29ec3564097be86465c0cf4ba7963f4c0
Gabriel Webb <gabrielwebb90@gmail.com>
2014-11-26 17:17:06 -0500

    changed text on first card
    says text while it fetches for the weather data. Data fectching has yet to be implemented

---
+++ src/app.js
@@ -0,0 +1,16 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+
+// Create a Card with title and subtitle
+var card = new UI.Card({
+  title:'Looking Outside',
+  subtitle:'hold up...'
+});
+
+// Display the Card
+card.show();
